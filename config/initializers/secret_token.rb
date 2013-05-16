8687e2aea1017fcb7206ee6f352bc5f80768cb7f
Chris Mailloux <chrismayu@gmail.com>
2013-05-16 13:12:47 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReefWatchApp::Application.config.secret_token = '6442cb5e92a6812828ca5e28fcdca21c99ae78c6e6eeed3b7e96888e93daf7d70736c11d1eb818b02e12c617de45f944a6f25f21207e4fcbb95b2f3053e2eb90'
